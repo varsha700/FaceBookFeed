6188876ce5fa0b9a6553f92672520d0c66031cbb
Varsha <60335007+varsha700@users.noreply.github.com>
2024-09-27 09:54:12 +0530

    Update urls.py

--- FeedApp/urls.py
+++ FeedApp/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.SHome, name='home'),
+    
     path('register/', views.register, name='register'),
     path('feed/', views.feed_view, name='feed'),
     path('post_comment/<int:message_id>/', views.post_comment, name='post_comment'),
@@ -15,4 +15,5 @@ urlpatterns = [
     path('delete_comment/<int:comment_id>/', views.delete_comment, name='delete_comment'),
     path('toggle_like/<int:message_id>/', views.toggle_like, name='toggle_like'),
     path('delete_post/<int:post_id>/', views.delete_post, name='delete_post'),
+    path('', views.home, name='home'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
